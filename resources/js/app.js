7dbe3fa02a666f6006c318ed40d160283f29ed61
mehdikord <mehdi.kord171@gmail.com>
2023-04-30 22:16:23 +0330

    Initial Commit( laravel 10 with vue js 3 with quasar )

---
+++ resources/js/app.js
@@ -0,0 +1,34 @@
+import {createApp} from "vue";
+import show from "./manage/template/show.vue";
+import { Quasar } from 'quasar';
+import quasarIconSet from 'quasar/icon-set/mdi-v7';
+// Import icon libraries
+import '@quasar/extras/roboto-font/roboto-font.css';
+import '@quasar/extras/fontawesome-v6/fontawesome-v6.css';
+// ..required because of selected iconSet:
+import '@quasar/extras/mdi-v7/mdi-v7.css';
+// Import Quasar css
+import 'quasar/dist/quasar.css';
+
+const App=createApp(show);
+
+App.use(Quasar, {
+    plugins: {}, // import Quasar plugins and add here
+    iconSet: quasarIconSet,
+    /*
+    config: {
+      brand: {
+        // primary: '#e46262',
+        // ... or all other brand colors
+      },
+      notify: {...}, // default set of options for Notify Quasar plugin
+      loading: {...}, // default set of options for Loading Quasar plugin
+      loadingBar: { ... }, // settings for LoadingBar Quasar plugin
+      // ..and many more (check Installation card on each Quasar component/directive/plugin)
+    }
+    */
+})
+
+
+
+App.mount('#app')
---
+++ resources/js/manage/template/show.vue
@@ -0,0 +1,65 @@
+<template>
+
+    <q-layout view="hHh lpr fFf">
+
+        <q-header elevated class="bg-primary text-white">
+            <q-toolbar>
+                <q-btn dense flat round icon="mdi-menu" @click="toggleLeftDrawer" />
+
+                <q-toolbar-title>
+                    <q-avatar>
+                        <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+                    </q-avatar>
+                    Title
+                </q-toolbar-title>
+
+<!--                <q-btn dense flat round icon="mdi-menu" @click="toggleRightDrawer" />-->
+            </q-toolbar>
+        </q-header>
+
+        <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
+            <!-- drawer content -->
+            test
+        </q-drawer>
+
+<!--        <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>-->
+<!--            &lt;!&ndash; drawer content &ndash;&gt;-->
+<!--            -->
+<!--        </q-drawer>-->
+
+        <q-page-container>
+
+        </q-page-container>
+
+    </q-layout>
+
+
+
+</template>
+
+<script>
+import {ref} from 'vue';
+export default {
+    name: "show",
+    setup () {
+        const leftDrawerOpen = ref(false)
+        const rightDrawerOpen = ref(false)
+
+        return {
+            leftDrawerOpen,
+            toggleLeftDrawer () {
+                leftDrawerOpen.value = !leftDrawerOpen.value
+            },
+
+            rightDrawerOpen,
+            toggleRightDrawer () {
+                rightDrawerOpen.value = !rightDrawerOpen.value
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
